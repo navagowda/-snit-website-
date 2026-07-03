d86ce18d2df69b54c97c98388e5a29814d4bd9ba
Nava <navagowdaas@gmail.com>
2026-07-03 13:02:53 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,346 @@
+export const gallery = [
+  {
+    "category": "Founder",
+    "src": "assets/images/founder-01.webp",
+    "thumb": "assets/images/founder-01-thumb.webp",
+    "title": "Founder 1",
+    "w": 1448,
+    "h": 1086
+  },
+  {
+    "category": "Founder",
+    "src": "assets/images/founder-02.webp",
+    "thumb": "assets/images/founder-02-thumb.webp",
+    "title": "Founder 2",
+    "w": 1229,
+    "h": 1536
+  },
+  {
+    "category": "Building",
+    "src": "assets/images/building-01.webp",
+    "thumb": "assets/images/building-01-thumb.webp",
+    "title": "Building 1",
+    "w": 1086,
+    "h": 1448
+  },
+  {
+    "category": "Building",
+    "src": "assets/images/building-02.webp",
+    "thumb": "assets/images/building-02-thumb.webp",
+    "title": "Building 2",
+    "w": 1448,
+    "h": 1086
+  },
+  {
+    "category": "Computer Lab",
+    "src": "assets/images/computer-lab-01.webp",
+    "thumb": "assets/images/computer-lab-01-thumb.webp",
+    "title": "Computer Lab 1",
+    "w": 1600,
+    "h": 1200
+  },
+  {
+    "category": "Computer Lab",
+    "src": "assets/images/computer-lab-02.webp",
+    "thumb": "assets/images/computer-lab-02-thumb.webp",
+    "title": "Computer Lab 2",
+    "w": 1600,
+    "h": 1204
+  },
+  {
+    "category": "Computer Lab",
+    "src": "assets/images/computer-lab-03.webp",
+    "thumb": "assets/images/computer-lab-03-thumb.webp",
+    "title": "Computer Lab 3",
+    "w": 1600,
+    "h": 1200
+  },
+  {
+    "category": "Computer Lab",
+    "src": "assets/images/computer-lab-04.webp",
+    "thumb": "assets/images/computer-lab-04-thumb.webp",
+    "title": "Computer Lab 4",
+    "w": 1600,
+    "h": 1204
+  },
+  {
+    "category": "Computer Lab",
+    "src": "assets/images/computer-lab-05.webp",
+    "thumb": "assets/images/computer-lab-05-thumb.webp",
+    "title": "Computer Lab 5",
+    "w": 1600,
+    "h": 1204
+  },
+  {
+    "category": "Computer Lab",
+    "src": "assets/images/computer-lab-06.webp",
+    "thumb": "assets/images/computer-lab-06-thumb.webp",
+    "title": "Computer Lab 6",
+    "w": 1600,
+    "h": 1204
+  },
+  {
+    "category": "Computer Lab",
+    "src": "assets/images/computer-lab-07.webp",
+    "thumb": "assets/images/computer-lab-07-thumb.webp",
+    "title": "Computer Lab 7",
+    "w": 1600,
+    "h": 1204
+  },
+  {
+    "category": "Computer Lab",
+    "src": "assets/images/computer-lab-08.webp",
+    "thumb": "assets/images/computer-lab-08-thumb.webp",
+    "title": "Computer Lab 8",
+    "w": 1600,
+    "h": 1200
+  },
+  {
+    "category": "Classroom",
+    "src": "assets/images/classroom-01.webp",
+    "thumb": "assets/images/classroom-01-thumb.webp",
+    "title": "Classroom 1",
+    "w": 1600,
+    "h": 1200
+  },
+  {
+    "category": "Classroom",
+    "src": "assets/images/classroom-02.webp",
+    "thumb": "assets/images/classroom-02-thumb.webp",
+    "title": "Classroom 2",
+    "w": 1600,
+    "h": 1204
+  },
+  {
+    "category": "Students",
+    "src": "assets/images/students-01.webp",
+    "thumb": "assets/images/students-01-thumb.webp",
+    "title": "Students 1",
+    "w": 1204,
+    "h": 1600
+  },
+  {
+    "category": "Students",
+    "src": "assets/images/students-02.webp",
+    "thumb": "assets/images/students-02-thumb.webp",
+    "title": "Students 2",
+    "w": 1204,
+    "h": 1600
+  },
+  {
+    "category": "Students",
+    "src": "assets/images/students-03.webp",
+    "thumb": "assets/images/students-03-thumb.webp",
+    "title": "Students 3",
+    "w": 1200,
+    "h": 1600
+  },
+  {
+    "category": "Students",
+    "src": "assets/images/students-04.webp",
+    "thumb": "assets/images/students-04-thumb.webp",
+    "title": "Students 4",
+    "w": 1204,
+    "h": 1600
+  },
+  {
+    "category": "Students",
+    "src": "assets/images/students-05.webp",
+    "thumb": "assets/images/students-05-thumb.webp",
+    "title": "Students 5",
+    "w": 1200,
+    "h": 1600
+  },
+  {
+    "category": "Students",
+    "src": "assets/images/students-06.webp",
+    "thumb": "assets/images/students-06-thumb.webp",
+    "title": "Students 6",
+    "w": 1204,
+    "h": 1600
+  },
+  {
+    "category": "Students",
+    "src": "assets/images/students-07.webp",
+    "thumb": "assets/images/students-07-thumb.webp",
+    "title": "Students 7",
+    "w": 1200,
+    "h": 1600
+  },
+  {
+    "category": "Abacus",
+    "src": "assets/images/abacus-01.webp",
+    "thumb": "assets/images/abacus-01-thumb.webp",
+    "title": "Abacus 1",
+    "w": 1516,
+    "h": 1010
+  },
+  {
+    "category": "Abacus",
+    "src": "assets/images/abacus-02.webp",
+    "thumb": "assets/images/abacus-02-thumb.webp",
+    "title": "Abacus 2",
+    "w": 1600,
+    "h": 1194
+  },
+  {
+    "category": "Abacus",
+    "src": "assets/images/abacus-03.webp",
+    "thumb": "assets/images/abacus-03-thumb.webp",
+    "title": "Abacus 3",
+    "w": 1600,
+    "h": 1200
+  },
+  {
+    "category": "Abacus",
+    "src": "assets/images/abacus-04.webp",
+    "thumb": "assets/images/abacus-04-thumb.webp",
+    "title": "Abacus 4",
+    "w": 1600,
+    "h": 1200
+  },
+  {
+    "category": "Events",
+    "src": "assets/images/events-01.webp",
+    "thumb": "assets/images/events-01-thumb.webp",
+    "title": "Events 1",
+    "w": 1600,
+    "h": 1200
+  },
+  {
+    "category": "Events",
+    "src": "assets/images/events-02.webp",
+    "thumb": "assets/images/events-02-thumb.webp",
+    "title": "Events 2",
+    "w": 1600,
+    "h": 1200
+  },
+  {
+    "category": "Events",
+    "src": "assets/images/events-03.webp",
+    "thumb": "assets/images/events-03-thumb.webp",
+    "title": "Events 3",
+    "w": 1600,
+    "h": 1200
+  },
+  {
+    "category": "Events",
+    "src": "assets/images/events-04.webp",
+    "thumb": "assets/images/events-04-thumb.webp",
+    "title": "Events 4",
+    "w": 1600,
+    "h": 1200
+  },
+  {
+    "category": "Events",
+    "src": "assets/images/events-05.webp",
+    "thumb": "assets/images/events-05-thumb.webp",
+    "title": "Events 5",
+    "w": 1600,
+    "h": 1200
+  },
+  {
+    "category": "Events",
+    "src": "assets/images/events-06.webp",
+    "thumb": "assets/images/events-06-thumb.webp",
+    "title": "Events 6",
+    "w": 1600,
+    "h": 1200
+  },
+  {
+    "category": "Events",
+    "src": "assets/images/events-07.webp",
+    "thumb": "assets/images/events-07-thumb.webp",
+    "title": "Events 7",
+    "w": 1600,
+    "h": 1200
+  },
+  {
+    "category": "Events",
+    "src": "assets/images/events-08.webp",
+    "thumb": "assets/images/events-08-thumb.webp",
+    "title": "Events 8",
+    "w": 1600,
+    "h": 1200
+  },
+  {
+    "category": "Certificates",
+    "src": "assets/images/certificates-01.webp",
+    "thumb": "assets/images/certificates-01-thumb.webp",
+    "title": "Certificates 1",
+    "w": 1600,
+    "h": 1265
+  },
+  {
+    "category": "Certificates",
+    "src": "assets/images/certificates-02.webp",
+    "thumb": "assets/images/certificates-02-thumb.webp",
+    "title": "Certificates 2",
+    "w": 1600,
+    "h": 1200
+  },
+  {
+    "category": "Gallery",
+    "src": "assets/images/gallery-01.webp",
+    "thumb": "assets/images/gallery-01-thumb.webp",
+    "title": "Gallery 1",
+    "w": 1600,
+    "h": 1200
+  },
+  {
+    "category": "Gallery",
+    "src": "assets/images/gallery-02.webp",
+    "thumb": "assets/images/gallery-02-thumb.webp",
+    "title": "Gallery 2",
+    "w": 1200,
+    "h": 1600
+  },
+  {
+    "category": "Gallery",
+    "src": "assets/images/gallery-03.webp",
+    "thumb": "assets/images/gallery-03-thumb.webp",
+    "title": "Gallery 3",
+    "w": 1200,
+    "h": 1600
+  },
+  {
+    "category": "Gallery",
+    "src": "assets/images/gallery-04.webp",
+    "thumb": "assets/images/gallery-04-thumb.webp",
+    "title": "Gallery 4",
+    "w": 1200,
+    "h": 1600
+  },
+  {
+    "category": "Gallery",
+    "src": "assets/images/gallery-05.webp",
+    "thumb": "assets/images/gallery-05-thumb.webp",
+    "title": "Gallery 5",
+    "w": 1200,
+    "h": 1600
+  },
+  {
+    "category": "Gallery",
+    "src": "assets/images/gallery-06.webp",
+    "thumb": "assets/images/gallery-06-thumb.webp",
+    "title": "Gallery 6",
+    "w": 1200,
+    "h": 1600
+  },
+  {
+    "category": "Gallery",
+    "src": "assets/images/gallery-07.webp",
+    "thumb": "assets/images/gallery-07-thumb.webp",
+    "title": "Gallery 7",
+    "w": 1599,
+    "h": 899
+  },
+  {
+    "category": "Gallery",
+    "src": "assets/images/gallery-08.webp",
+    "thumb": "assets/images/gallery-08-thumb.webp",
+    "title": "Gallery 8",
+    "w": 1600,
+    "h": 1204
+  }
+];
